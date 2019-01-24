058af615435f7a4acd3011c63fbcdfd6d6be7048
Your Name <you@example.com>
2019-01-24 17:12:58 +0000

    Remove code for models which aren't in paper

--- Productivity_Diversity_SAR.R
+++ Productivity_Diversity_SAR.R
@@ -3,7 +3,7 @@
 ####### Productivity-diversity model for entire dataset - area included #######
 ###############################################################################
 
-## HAnnah White 14.11.2018
+## Hannah White 14.11.2018
 
 ## This code carries out the productivity diversity relationships but includes an area term to account for the species-area relationship. The z value 0.08 was determined from an nls
 
@@ -56,55 +56,11 @@ vasc.env$non.past <- 1 - vasc.env$Pasture
 
 vasc.env$non.past.area <- 100*vasc.env$non.past
 
-vasc.env$area.0.08 <- vasc.env$non.past.area^0.08
 
 
 rm(corine.agg)
 
 
-###
-
-
-m.full.area <- 
-  gamm(nat.fres ~ s(evi.mean,  bs = 'cs', k = -1) + 
-         s(evi.sd, bs = 'cs', k = -1) +
-         s(inter.evi.sd, bs = 'cs', k = -1) +
-         s(annual.evi.sd, bs = 'cs', k = -1) +
-         te(east, north, bs = c('tp', 'tp')) +
-         area.0.08, 
-       correlation = corExp(form = ~ east + north), data = vasc.env, 
-       method = 'ML', control = lmeControl(opt = 'optim'))
-
-## AIC = 6799.005
-
-
-
-m.full.exp <- 
-  gamm(nat.fres ~ s(evi.mean,  bs = 'cs', k = -1) + 
-         s(evi.sd, bs = 'cs', k = -1) +
-         s(inter.evi.sd, bs = 'cs', k = -1) +
-         s(annual.evi.sd, bs = 'cs', k = -1) +
-         te(east, north, bs = c('tp', 'tp')), 
-       correlation = corExp(form = ~ east + north), data = vasc.env, 
-       method = 'ML', control = lmeControl(opt = 'optim'))
-
-
-## AIC = 6843.558
-
-
-
-
-m.full.dom3level.area <- gamm(nat.fres ~ s(evi.mean,  bs = 'cs', by = as.factor(hetpast), k = -1) + 
-                               s(evi.sd, bs = 'cs', by = as.factor(hetpast), k = -1) +
-                               s(inter.evi.sd, bs = 'cs', by = as.factor(hetpast), k = -1) +
-                               s(annual.evi.sd, bs = 'cs', by = as.factor(hetpast), k = -1) +
-                               te(east, north, bs = c('tp', 'tp')) + hetpast +
-                               area.0.08, 
-                             correlation = corExp(form = ~ east + north), data = vasc.env, 
-                             method = 'ML', control = lmeControl(opt = 'optim'))
-
-
-## AIC = 6788.297
 
 
 #### Allow GAM to smooth area term
@@ -124,19 +80,6 @@ m.full.area2 <-
 ## AIC = 6733.796
 
 
-m.full.area3 <-  # same as previous model but reduces number of knots
-  gamm(nat.fres ~ s(evi.mean,  bs = 'cs', k = -1) + 
-         s(evi.sd, bs = 'cs', k = -1) +
-         s(inter.evi.sd, bs = 'cs', k = -1) +
-         s(annual.evi.sd, bs = 'cs', k = -1) +
-         te(east, north, bs = c('tp', 'tp')) +
-         s(non.past.area, bs = 'cs', k = 3), 
-       correlation = corExp(form = ~ east + north), data = vasc.env, 
-       method = 'ML', control = lmeControl(opt = 'optim'))
-
-## AIC = 6754.801
-
-
 
 m.full.dom3level.area2 <- gamm(nat.fres ~ s(evi.mean,  bs = 'cs', by = as.factor(hetpast), k = -1) + 
                                 s(evi.sd, bs = 'cs', by = as.factor(hetpast), k = -1) +
@@ -154,71 +97,7 @@ m.full.dom3level.area2 <- gamm(nat.fres ~ s(evi.mean,  bs = 'cs', by = as.factor
 
 
 
-## check order
 
-m.full.dom3level.areatop <- gamm(nat.fres ~ s(non.past.area, bs = 'cs', k = -1) +
-                                 s(evi.mean,  bs = 'cs', by = as.factor(hetpast), k = -1) + 
-                                 s(evi.sd, bs = 'cs', by = as.factor(hetpast), k = -1) +
-                                 s(inter.evi.sd, bs = 'cs', by = as.factor(hetpast), k = -1) +
-                                 s(annual.evi.sd, bs = 'cs', by = as.factor(hetpast), k = -1) +
-                                 te(east, north, bs = c('tp', 'tp')) + hetpast, 
-                               correlation = corExp(form = ~ east + north), data = vasc.env, 
-                               method = 'ML', control = lmeControl(opt = 'optim')) ## does not converge
-
-## AIC = 6745.789
-
-
-## Order covariates are in model does not make a difference
-
-## null
-
-m0 <- gam(nat.fres ~  1 + te(east, north, bs = c('tp', 'tp'), sp = m.full.dom3level.area2$sp[13:14]),
-                             correlation = corExp(form = ~ east + north), data = vasc.env, method = 'ML')
-
-
-
-
-## Contribution of each whilst maintaining original smooth - I think this might be impossible
-
-m.nomean <- gamm(nat.fres ~ # nope this isn't maintaining smooth
-                   s(evi.sd, bs = 'cs', by = as.factor(hetpast), k = -1) +
-                   s(inter.evi.sd, bs = 'cs', by = as.factor(hetpast), k = -1) +
-                   s(annual.evi.sd, bs = 'cs', by = as.factor(hetpast), k = -1) +
-                   te(east, north, bs = c('tp', 'tp')) + hetpast +
-                   s(non.past.area, bs ='cs', k = -1), 
-                 correlation = corExp(form = ~ east + north), data = vasc.env, 
-                 method = 'ML', control = lmeControl(opt = 'optim'), sp = m.full.dom3level.area2$gam$sp[4:16])
-# AIc = 6739.612
-
-
-
-
-#m.nomean <- gam(nat.fres ~  ## this is wrong as gam ignores the correlation structure
-#                  s(evi.sd, bs = 'cs', by = as.factor(hetpast), k = -1, sp = m.full.dom3level.area2$sp[4:6]) +
-#                  s(inter.evi.sd, bs = 'cs', by = as.factor(hetpast), k = -1, sp = m.full.dom3level.area2$sp[7:9]) +
-#                  s(annual.evi.sd, bs = 'cs', by = as.factor(hetpast), k = -1, sp = m.full.dom3level.area2$sp[10:12]) +
-#                  te(east, north, bs = c('tp', 'tp'), sp = m.full.dom3level.area2$sp[13:14]) + hetpast +
-#                  s(non.past.area, bs ='cs', k = -1, sp = m.full.dom3level.area2$sp[15]), 
-#                correlation = corExp(form = ~ east + north), data = vasc.env, 
-#                method = 'ML')
-# AIc = 6961.678
-
-
-m.nomean.nonspec <- gamm(nat.fres ~ 
-                           s(evi.sd, bs = 'cs', by = as.factor(hetpast), k = -1) +
-                           s(inter.evi.sd, bs = 'cs', by = as.factor(hetpast), k = -1) +
-                           s(annual.evi.sd, bs = 'cs', by = as.factor(hetpast), k = -1) +
-                           te(east, north, bs = c('tp', 'tp')) + hetpast +
-                           s(non.past.area, bs ='cs', k = -1), 
-                         correlation = corExp(form = ~ east + north), data = vasc.env, 
-                         method = 'ML', control = lmeControl(opt = 'optim'))
-# AIc = 6739.612
-
-
-
-
-
-(deviance(m.nomean$gam)-deviance(m.full.dom3level.area2$gam))/deviance(m0)
 
 
 
